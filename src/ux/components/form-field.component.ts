b6f3539bd5ad337657e17db9d82cf1d256514ed9
Couiller Celia <celia.couiller@opendigitaleducation.com>
2020-05-18 15:35:46 +0200

    feat(crm): Add a required option to the form-field component

--- src/ux/components/form-field.component.ts
+++ src/ux/components/form-field.component.ts
@@ -6,6 +6,7 @@ import { Component, Input } from '@angular/core'
         <div class="form-field">
             <label>
                 {{ label | translate }}
+                <span *ngIf="required" class="asterisque">*</span>
                 <message-sticker *ngIf="help" 
                             [type]="'info'" 
                             [messages]="[help]">
@@ -19,9 +20,11 @@ import { Component, Input } from '@angular/core'
         div.form-field >>> > * { flex: 1; margin-left: 5px; }
         div.form-field > *:first-child { flex: 0 0 200px; margin-left: 0; }
         div.form-field message-sticker { font-size: 1.3em; }
+        div.form-field .asterisque { color: crimson; } 
     `]
 })
 export class FormFieldComponent {
     @Input() label: string;
     @Input() help: string;
+    @Input() required: boolean;
 }
